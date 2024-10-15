7941d142e07c03613e63e149892991a0d80340de
Chad Estoup--Streiff <chadestoupstreiff@gmail.com>
2024-10-15 10:39:20 +0200

    complete code

--- src/main.py
+++ src/main.py
@@ -3,35 +3,46 @@ import time
 import numpy as np
 from printer import print_table
 from solver import solve
+from solver_copy import solve as solve2
 from tabler import read_table
 
 files = [
     # "input_files/s0.txt",
     "input_files/s1.txt",
-    "input_files/s2.txt",
-    "input_files/s3.txt",
-    "input_files/s4.txt",
-    "input_files/s5.txt",
-    "input_files/s6.txt",
-    "input_files/s7.txt",
-    "input_files/s8.txt",
-    "input_files/s9.txt",
-    "input_files/s10.txt",
-    "input_files/s11.txt",
-    "input_files/s12.txt",
+    # "input_files/s2.txt",
+    # "input_files/s3.txt",
+    # "input_files/s4.txt",
+    # "input_files/s5.txt",
+    # "input_files/s6.txt",
+    # "input_files/s7.txt",
+    # "input_files/s8.txt",
+    # "input_files/s9.txt",
+    # "input_files/s10.txt",
+    # "input_files/s11.txt",
+    # "input_files/s12.txt",
 ]
 
 # sys.setrecursionlimit(9999999)
 
 for file in files:
-    for i in range(8, 12):
+    for i in range(8, 13):
         print(f"\nSolving {file} with {i} limit...")
         table = read_table(file)[:i, :i]
         print_table(table)
 
         start = time.time()
-        solution = solve(table, tqdm_disable=False)
+        solution = solve(table.copy(), tqdm_disable=False)
         end = time.time()
-
         print(f"Solution with {np.max(solution)} in {end - start:4f}s:")
         print_table(solution)
+
+        start = time.time()
+        solution_copy = solve2(table.copy(), tqdm_disable=False)
+        end = time.time()
+        print(f"BIS >> Solution with {np.max(solution_copy)} in {end - start:4f}s:")
+        print_table(solution_copy)
+
+        if not np.array_equal(solution_copy, solution):
+            print("NOT SAME OUTPUT")
+            print(solution - solution_copy)
+            exit()
