5149458d3bf44a51a14f716b81f7fdc996ee161a
Chad Estoup--Streiff <chadestoupstreiff@gmail.com>
2024-10-16 16:37:29 +0200

    Readme update

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # cimpress
-Launch algorithme:
-
+## 🚀 Launch solver:
 ```bash
 python src/main.py
 ```
+
+## ⚙️ Implemented solutions
+Each recursion goes deeper inside tables possibilities. The goal is to reduce the number of checked possibilities to be runable in a quick enought time.
+For each recursion we do:
+- We fill every corner that can't expand (isolated ones) to remove 1 recursion for each one.
+- We create a recursion for each corner in the new table to avoir creating a recursion for each free space as bigger square allways touch at least one corner.
+- To find the bigger one, we created a square on the corner that expands until he can't.
+- We have created a memory of seen table composition. Before launching the recursion on the new table after adding a square in corner we check if the created table has already been seen and if it does, we don't launch the next recursion to avoid useless redundant checks. Speeds up a lot.
+- We added a max_authorized_index, that allways fit to the maximum of solution during algorithme, to stop recursion calls if it get higher index of squares as solution will not be better and be useless calls.
+
+## 🔍 Results
+### Solution for: s1.txt
+![sols1](photos/s1.png)
+
--- src/main.py
+++ src/main.py
@@ -3,14 +3,12 @@ import time
 import numpy as np
 from printer import print_table
 from solver import solve
-# from solver_copy import solve as solve2
 from tabler import read_table
 
 files = [
-    # "input_files/s0.txt",
-    "input_files/s1.txt",
-    "input_files/s2.txt",
-    "input_files/s3.txt",
+    # "input_files/s1.txt",
+    # "input_files/s2.txt",
+    # "input_files/s3.txt",
     "input_files/s4.txt",
     "input_files/s5.txt",
     "input_files/s6.txt",
@@ -22,8 +20,6 @@ files = [
     "input_files/s12.txt",
 ]
 
-# sys.setrecursionlimit(9999999)
-
 for file in files:
     print(f"\nSolving {file}...")
     table = read_table(file)
@@ -32,16 +28,6 @@ for file in files:
     start = time.time()
     solution = solve(table.copy(), tqdm_disable=False)
     end = time.time()
-    print(f"Solution with {np.max(solution)} in {end - start:4f}s:")
+    print(f"Solution with {np.max(solution)} squares in {(end - start):.4f}s:")
     print_table(solution)
 
-    # start = time.time()
-    # solution_copy = solve2(table.copy(), tqdm_disable=False)
-    # end = time.time()
-    # print(f"BIS >> Solution with {np.max(solution_copy)} in {end - start:4f}s:")
-    # print_table(solution_copy)
-
-    # if not np.array_equal(solution_copy, solution):
-    #     print("NOT SAME OUTPUT")
-    #     print(solution - solution_copy)
-    #     exit()
